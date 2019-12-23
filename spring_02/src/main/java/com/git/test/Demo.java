fb6235ff57da3558bc7865f6761bfa04af92a466
炜哥哥 <https://gitee.com/studayOnQF/FengMiNet.git>
2019-12-23 11:15:29 +0800

    this a git test

---
+++ spring_02/src/main/java/com/git/test/Demo.java
@@ -0,0 +1,16 @@
+package com.git.test;
+
+/**
+ * @author 炜哥哥
+ * @date 2019/12/23 11:05
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("this is a git test");
+        System.out.println("haha");
+    }
+    public void test(){
+        System.out.println("jiji");
+    }
+
+}
